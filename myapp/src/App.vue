4ec09441b5d3e29de2ddc764006921423b28a4ed
Jason <18665308713@163.com>
2020-08-09 13:55:29 +0800

    topnav 样式更新

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -1,23 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
-  <div>nav | 
-    <router-link to="/"> frank</router-link> |
-    <router-link to="/xxx"> frank2</router-link>
-  
+  <div>
+    <router-view></router-view>
   </div>
-  <router-view />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import Frank from './components/Frank.vue'
-
-
-export default {
-  name: 'App',
-  components: {
-    Frank
+  export default {
+    name: 'app'
   }
-}
 </script>
+
--- myapp/src/main.ts
+++ myapp/src/main.ts
@@ -1,17 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './index.css'
+import './index.scss'
 
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Frank from './components/Frank.vue';
-import Frank2 from './components/Frank2.vue';
+import Home from './view/Home.vue';
+import Doc from './view/Doc.vue';
 
 const history  = createWebHashHistory();
 const router = createRouter({
     history: history,
     routes: [
-        {path: '/', component: Frank },
-        {path: '/xxx', component: Frank2 },
+        {path: '/', component: Home },
+        {path: '/xxx', component: Home },
     ]
 });
 
