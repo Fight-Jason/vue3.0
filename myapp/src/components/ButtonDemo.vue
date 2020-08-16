f56e9dabe76c5931f6b9d723bce5d05b867c7560
Jason <18665308713@163.com>
2020-08-16 16:03:34 +0800

    add Button component and css

---
+++ myapp/src/components/ButtonDemo.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <Button 
+            theme="button"
+        >123 
+        </Button>
+        <Button 
+            theme="link"
+        >123 
+        </Button>
+
+    </div>
+</template>
+
+<script lang="ts">
+import Button from "../lib/Button.vue"
+export default {
+    name: 'ButtonDemo',
+    components: {
+        Button
+    },
+    methods: {
+        handleClick() {
+            console.log("handleClick")
+        },
+        handleMouseover() {
+            console.log("handleMouseover")
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- myapp/src/router.ts
+++ myapp/src/router.ts
@@ -3,7 +3,7 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from './view/Home.vue';
 import Doc from './view/Doc.vue';
 import Switch from './components/switchDemo.vue';
-import Button from './components/Button.vue';
+import Button from './components/ButtonDemo.vue';
 import Dialog from './components/Dialog.vue';
 import Tabs from './components/Tabs.vue';
 
