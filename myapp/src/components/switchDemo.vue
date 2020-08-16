c34e0c63f6823d9878f5c23969270fcec2e69ed1
Jason <18665308713@163.com>
2020-08-16 10:12:06 +0800

    add switch style

---
+++ myapp/src/components/switchDemo.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <Switch />
+    </div>
+</template>
+
+<script lang="ts">
+import Switch from "../lib/switch.vue"
+export default {
+    name: 'switchDemo',
+    components: {
+        Switch
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
