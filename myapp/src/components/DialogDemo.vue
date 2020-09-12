535951a9653d2e7351c2dcb7ae96421710b4f849
Jason <18665308713@163.com>
2020-09-12 19:07:55 +0800

    add dialog styles make visible attribute logic

--- myapp/src/components/DialogDemo.vue
+++ myapp/src/components/DialogDemo.vue
@@ -1,15 +1,30 @@
 <template>
-<div>
-    <Dialog />
-</div>
+<div>Dialog 示例</div>
+<Button @click="toggle">Toggle </Button>
+<Dialog :visible="x" />
 </template>
 
 <script>
 import Dialog from '../lib/Dialog.vue'
+import Button from '../lib/Button.vue'
+import {
+  ref
+} from 'vue'
 export default {
-    component: {
-        Dialog
+  components: {
+    Dialog,
+    Button
+  },
+  setup() {
+    const x = ref(false)
+    const toggle = () => {
+      x.value = !x.value
     }
+    return {
+      x,
+      toggle
+    }
+  }
 }
 </script>
 
