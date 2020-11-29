cde053e670482e924e53ce4a8083dda3072a08ac
Jason <18665308713@163.com>
2020-11-29 15:08:45 +0800

    引入 github-markdown 样式

--- myapp/src/view/Intro.vue
+++ myapp/src/view/Intro.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
-    介绍
-  </div>
+  <article class="markdown-body">
+    <h1>介绍</h1>
+    <p>King UI 是一款基于 Vue 3 和TypeScript 的 UI 组件库。</p>
+    <p>下一节：<a href="#/doc/install">安装</a></p>
+  </article>
 </template>
 
 <script>
