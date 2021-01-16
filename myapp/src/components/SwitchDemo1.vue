d5d15fc1656e7943d34466db3916a87838559e16
Jason <18665308713@163.com>
2021-01-16 15:26:01 +0800

    展示组件源代码

---
+++ myapp/src/components/SwitchDemo1.vue
@@ -0,0 +1,30 @@
+<demo>
+常规用法
+</demo>
+<template>
+    <div>
+        <Switch v-model:value="bool" />
+    </div>
+</template>
+
+<script lang="ts">
+import Switch from '../lib/Switch.vue'
+import {
+  ref
+} from 'vue'
+export default {
+  components: {
+    Switch
+  },
+  setup() {
+    const bool = ref(false)
+    return {
+      bool
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
