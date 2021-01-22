8d79eea29ea08215ec374197a9cec752b013b8b4
Jason <18665308713@163.com>
2021-01-22 21:24:40 +0800

    删除辣鸡代码

--- myapp/src/components/switchDemo.vue
+++ myapp/src/components/switchDemo.vue
@@ -8,33 +8,22 @@
 </template>
 
 <script lang="ts">
-import Switch from '../lib/Switch.vue';
-import Button from '../lib/Button.vue';
 import SwitchDemo1 from './SwitchDemo1.vue';
 import SwitchDemo2 from './SwitchDemo2.vue';
 import Demo from './Demo.vue';
-import 'prismjs';
-import 'prismjs/themes/prism.css'
-const Prism = (window as any).Prism
 import {
   ref
 } from 'vue'
 export default {
   components: {
-    Switch,
-    Button,
     Demo,
     SwitchDemo1,
     SwitchDemo2
   },
   setup() {
-    const bool = ref(false);
-    console.log(Prism)
     return {
-      bool,
       SwitchDemo1,
       SwitchDemo2,
-      Prism
     }
   }
 }
