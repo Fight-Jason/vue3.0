8a3db1a79e41ca4f3dfcf1766c9d03faef3b92dc
Jason <18665308713@163.com>
2021-01-24 10:08:33 +0800

    优化dialog 组件

--- myapp/src/components/Dialog1.demo.vue
+++ myapp/src/components/Dialog1.demo.vue
@@ -4,7 +4,7 @@
 <template>
 <div>
   <Button @click="toggle">打开对话框</Button>
-  <Dialog v-model:visible="x" :closeOnClickOverlay="false" :ok="f1" :cancel="f2">
+  <Dialog v-model:visible="x" :closeOnClickOverlay="false" :confirm="f1" :cancel="f2">
     <template v-slot:content>
       <strong>hi</strong>
       <div>hi2</div>
@@ -36,7 +36,7 @@ export default {
       x.value = !x.value
     }
     const f1 = () => {
-      return false
+      return true
     }
     const f2 = () => {}
     return {
